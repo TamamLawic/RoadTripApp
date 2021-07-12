1a326e1182426f6993e0ccfb709a136f6d1c09e4
TamamLawic <svanpelt@fb.com>
2021-07-12 15:44:56 -0600

    Added Bottom Navigation view, and starter map

--- app/src/main/java/com/example/roadtripapp_fbu/MainActivity.java
+++ app/src/main/java/com/example/roadtripapp_fbu/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.roadtripapp_fbu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.roadtripapp_fbu.Fragments.MapsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.action_feed:
+                                fragment = new MapsFragment();
+                                break;
+                            case R.id.action_create_trip:
+                                fragment = new MapsFragment();
+                                break;
+                            case R.id.action_profile:
+                            default:
+                                fragment = new MapsFragment();
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_feed);
     }
 }
